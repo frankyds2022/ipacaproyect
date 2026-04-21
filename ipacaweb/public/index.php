c9a05f5856ab4f092d4404b925cace76b6c6a962
FrankyDS <105567479+frankyds2022@users.noreply.github.com>
2026-04-21 10:15:14 -0500

    Auto-detect environment for index.php

--- ipacaweb/public/index.php
+++ ipacaweb/public/index.php
@@ -5,17 +5,24 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Define logical paths to check environment
+$hostingerPath = __DIR__.'/../ipaca_web';
+$localPath = __DIR__.'/..';
+
+// Detect if we are on Hostinger (sibling ipaca_web exists) or Local
+$basePath = is_dir($hostingerPath) ? $hostingerPath : $localPath;
+
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = $basePath.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../ipaca_web/vendor/autoload.php';
+require $basePath.'/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../ipaca_web/bootstrap/app.php';
+$app = require_once $basePath.'/bootstrap/app.php';
 $app->usePublicPath(__DIR__);
 
 $app->handleRequest(Request::capture());
