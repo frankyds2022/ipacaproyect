d315132bba1f93223aa4938b8866aac8c7e7b835
FrankyDS <105567479+frankyds2022@users.noreply.github.com>
2026-04-21 11:42:41 -0500

    Ultra-safe professional formatting for contact email template

--- ipacaweb/resources/views/emails/contact-message.blade.php
+++ ipacaweb/resources/views/emails/contact-message.blade.php
@@ -5,31 +5,25 @@ Estimado equipo de **IPACA Consultores**,
 
 Se ha registrado el ingreso de una nueva solicitud a través del portal web oficial. A continuación, se detallan los datos del prospecto para su pronta atención:
 
-<x-mail::table>
-| Datos del Prospecto | Información Registrada |
-| :--- | :--- |
-| **Peticionario / Empresa:** | {{ $data['name'] }} |
-| **Número de Contacto:** | {{ $data['phone'] }} |
-| **Correo Electrónico:** | [{{ $data['email'] }}](mailto:{{ $data['email'] }}) |
-| **Servicio de Interés:** | {{ $data['service'] ? $data['service'] : 'No especificado (Asesoría General)' }} |
-</x-mail::table>
-
-<br>
-
-### 📄 Detalles Adicionales y Descripción
-
 <x-mail::panel>
-**Mensaje original escrito por el cliente:**  
-*"{!! nl2br(e($data['message'])) !!}"*
+### 👤 Información Estratégica del Cliente
+
+* **Peticionario / Empresa:** {{ $data['name'] }}
+* **Número de Contacto:** {{ $data['phone'] }}
+* **Correo Electrónico:** [{{ $data['email'] }}](mailto:{{ $data['email'] }})
+* **Servicio de Interés:** {{ $data['service'] ?: 'No especificado (Asesoría General)' }}
 </x-mail::panel>
 
-Para brindar un seguimiento inmediato, puede responder directamente a este correo haciendo clic en el siguiente botón:
+### 📄 Descripción y Detalles de la Petición:
+> {{ $data['message'] }}
+
+Para agilizar el proceso y brindar un seguimiento inmediato, puede responder directamente a este mensaje haciendo clic en el siguiente botón:
 
 <x-mail::button :url="'mailto:'.$data['email']" color="error">
-Responder a {{ $data['name'] }}
+Responder al Prospecto
 </x-mail::button>
 
 Atentamente,<br>
-**Sistema Automático de {{ config('app.name') }}**  
-*(Por favor, atienda esta solicitud en las próximas 24 horas hábiles).*
+**Sistema Central de {{ config('app.name', 'Ipaca Consultores') }}**  
+*(Por favor, atienda esta solicitud en las próximas 24 h. hábiles)*
 </x-mail::message>
